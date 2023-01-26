ee0dcade3e1be3fe807fe07eedea238051e57663
Maria Luiza <marialuiza.almeidafernandes@gmail.com>
2023-01-26 07:28:22 -0300

    main ts changes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import * as hbs from 'express-handlebars';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
